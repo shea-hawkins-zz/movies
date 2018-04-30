cc2fda33c61c6dd5890d54c06882c00543a247dd
Shea Hawkins <shea.m.hawkins@gmail.com>
2018-04-29 20:11:57 -0700

    Add specs for character route

--- test/StarWarsControllerSpec.scala
+++ test/StarWarsControllerSpec.scala
@@ -3,7 +3,7 @@ import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.http.Status
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
-import star_wars.models.StarWarsMoviesByDirector
+import star_wars.models.{StarWarsCharacter, StarWarsMoviesByDirector}
 
 /**
  * Functional tests start a Play application internally, available
@@ -13,22 +13,39 @@ class StarWarsControllerSpec extends PlaySpec with GuiceOneAppPerSuite {
     "getMovieByDirector" should {
         "return a valid response" in {
             // Checks that the route functions and it provides the correct response format
-            route(app, FakeRequest(GET, "/star_wars/movies")).map(contentAsJson).flatMap(
+            val response = route(app, FakeRequest(GET, "/star_wars/movies")).map(contentAsJson).flatMap(
                 json => {
                     (json \ "results").asOpt[Seq[StarWarsMoviesByDirector]]
                 }
-            ).isDefined mustBe true
+            )
+
+            response.isDefined mustBe true
+            response.getOrElse(Nil).nonEmpty mustBe true
         }
     }
 
     "getCharactersByMovie" should {
         "return a valid response when provided a valid id" in {
-            true mustBe true
+            val response = route(app, FakeRequest(GET, "/star_wars/movies/1/characters")).map(contentAsJson).flatMap(
+                json => {
+                    (json \ "results").asOpt[Seq[StarWarsCharacter]]
+                }
+            )
+
+            response.isDefined mustBe true
+            response.getOrElse(Nil).nonEmpty mustBe true
         }
 
         "return a not found response when provided an invalid id" in {
+                                                           // see you in 2200
+            val response = route(app, FakeRequest(GET, "/star_wars/movies/42/characters"))
 
-            true mustBe true
+            response.map(status).getOrElse(0) mustBe 404
+            response.map(contentAsJson).flatMap(
+                json => {
+                    (json \ "results").asOpt[Seq[StarWarsCharacter]]
+                }
+            ).get.isEmpty mustBe true
         }
     }
 }
